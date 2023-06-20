077479e10f5360688d9c81d64035c3bd0d20af55
John <kimo87@gmail.com>
2023-06-21 01:46:34 +0900

    페이징 작업
    페이징 작업

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 
 @RequiredArgsConstructor
 @Service
@@ -24,6 +30,14 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
+    
+    public Page<Question> getList(int page) {
+    	List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10,Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
+
     
 
     public void create(String subject, String content) {
