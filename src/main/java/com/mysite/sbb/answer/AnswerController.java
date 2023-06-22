aff0f3183975327d928136f39824a0f1b76b43ec
John <kimo87@gmail.com>
2023-06-22 16:40:39 +0900

    답변 오류수정
    답변 오류수정

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -83,7 +83,7 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
         }
         this.answerService.delete(answer);
-        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(),answer.getId());
     }
     
     @PreAuthorize("isAuthenticated()")
